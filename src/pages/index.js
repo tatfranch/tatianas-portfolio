419a4126c855f5df5f69fad21ece3005ce45eb8e
Tatiana Paga Franch <tatianapagafranch@gmail.com>
2022-02-16 13:14:13 +0100

    pics added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Homepage = () => {
   return (
     <>
       <Layout pageTitle="Home" pageHeading="This is my portfolio">
-        Welcome to my cool site.
+        <p>Welcome to my cool site.</p>
+        <StaticImage
+          src="https://images.unsplash.com/photo-1492998680170-81f8863d8d2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2050&q=80"
+          alt="Japanese lamps"
+        />
+        {/* <StaticImage src="../images/Timisito.jpg" alt="cat illustration" /> */}
       </Layout>
     </>
   );
